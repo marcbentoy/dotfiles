c63b5cc963939810131316d9b4a9e7cc63aedcfe
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-10-23 18:14:40 +0800

    up: bufferline navigation keymap

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -21,18 +21,10 @@ map("n", "<leader>[", "<C-o>", { desc = "jump back", noremap = true, silent = tr
 map("n", "<leader>]", "<C-i>", { desc = "jump forward", noremap = true, silent = true })
 
 -- Navigate to the next buffer
-map("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+map("n", "<A-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 
 -- Navigate to the previous buffer
-map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-
--- Function to close current buffer and move to the next one
-function CloseCurrentBuffer()
-	local current_buffer = vim.api.nvim_get_current_buf()
-	vim.cmd("bd")
-	-- Move to the next buffer
-	vim.cmd("bnext")
-end
+map("n", "<A-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
 
 -- Map the function to a key (e.g., <leader>c)
 vim.api.nvim_set_keymap("n", "<leader>x", ":lua CloseCurrentBuffer()<CR>", { noremap = true, silent = true })
@@ -63,16 +55,16 @@ map("n", "<leader>wk", ":wincmd k<CR>", { desc = "Jump to up window" })
 map("n", "<leader>wl", ":wincmd l<CR>", { desc = "Jump to right window" })
 
 for i = 1, 9, 1 do
-	-- Move between windows by leader + window number
-	map("n", "<leader>" .. i, ":" .. i .. "wincmd w<CR>")
-	-- Close a window by leader + d + window number
-	map("n", "<leader>d" .. i, ":" .. i .. "wincmd c<CR>")
+    -- Move between windows by leader + window number
+    map("n", "<leader>" .. i, ":" .. i .. "wincmd w<CR>")
+    -- Close a window by leader + d + window number
+    map("n", "<leader>d" .. i, ":" .. i .. "wincmd c<CR>")
 end
 
-map("n", "=", "<cmd>vertical resize +5<cr>", { desc = "Make window wider (horizontally)" }) -- make the window biger vertically
+map("n", "=", "<cmd>vertical resize +5<cr>", { desc = "Make window wider (horizontally)" })      -- make the window biger vertically
 map("n", "-", [[<cmd>vertical resize -5<cr>]], { desc = "Make window narrower (horizontally)" }) -- make the window smaller vertically
-map("n", "+", [[<cmd>horizontal resize +2<cr>]], { desc = "Make window taller (vertically)" }) -- make the window bigger horizontally by pressing shift and =
-map("n", "_", [[<cmd>horizontal resize -2<cr>]], { desc = "Make window shorter (vertically)" }) -- make the window smaller horizontally by pressing shift and -
+map("n", "+", [[<cmd>horizontal resize +2<cr>]], { desc = "Make window taller (vertically)" })   -- make the window bigger horizontally by pressing shift and =
+map("n", "_", [[<cmd>horizontal resize -2<cr>]], { desc = "Make window shorter (vertically)" })  -- make the window smaller horizontally by pressing shift and -
 
 -- Tabs
 map("n", "<leader>tt", ":tabnew<CR>")
@@ -96,11 +88,11 @@ map("n", "<leader>bda", ":%bd<CR>", { desc = "Close all buffers" })
 
 -- todo-comment plugin
 vim.keymap.set("n", "]t", function()
-	require("todo-comments").jump_next()
+    require("todo-comments").jump_next()
 end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", "[t", function()
-	require("todo-comments").jump_prev()
+    require("todo-comments").jump_prev()
 end, { desc = "Previous todo comment" })
 
 vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>", { desc = "Toggle Comment" })
@@ -110,13 +102,13 @@ map("n", "<leader>m", ":Format<CR>", { desc = "Format file" })
 map("v", "<leader>m", ":Format<CR>", { desc = "Format file" })
 
 vim.api.nvim_create_user_command("Format", function(args)
-	local range = nil
-	if args.count ~= -1 then
-		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-		range = {
-			start = { args.line1, 0 },
-			["end"] = { args.line2, end_line:len() },
-		}
-	end
-	require("conform").format({ async = true, lsp_fallback = true, range = range })
+    local range = nil
+    if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+        }
+    end
+    require("conform").format({ async = true, lsp_fallback = true, range = range })
 end, { range = true })
