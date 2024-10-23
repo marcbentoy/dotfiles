690619d384d3553e420aa5ccad53056f0542834f
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-10-23 16:42:13 +0800

    up: disable tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,99 +3,99 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration
 local config = wezterm.config_builder()
-local dimmer = { brightness = 0.02 }
+-- local dimmer = { brightness = 0.02 }
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 12
 
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = true
+config.enable_tab_bar = false
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	bottom = 0,
-	top = 0,
+    left = 0,
+    right = 0,
+    bottom = 0,
+    top = 0,
 }
 
 -- tmux
 config.leader = { key = "a", mods = "ALT", timeout_milliseconds = 2000 }
 config.keys = {
-	{
-		mods = "ALT",
-		key = "c",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		mods = "ALT",
-		key = "x",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	-- {
-	-- 	mods = "ALT|SHIFT",
-	-- 	key = "h",
-	-- 	action = wezterm.action.ActivateTabRelative(-1),
-	-- },
-	-- {
-	-- 	mods = "ALT|SHIFT",
-	-- 	key = "l",
-	-- 	action = wezterm.action.ActivateTabRelative(1),
-	-- },
-	{
-		mods = "ALT",
-		key = "\\",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "ALT",
-		key = "-",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "ALT",
-		key = "h",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		mods = "ALT",
-		key = "j",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		mods = "ALT",
-		key = "k",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		mods = "ALT",
-		key = "l",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		mods = "ALT",
-		key = "LeftArrow",
-		action = wezterm.action.AdjustPaneSize({ "Left", 3 }),
-	},
-	{
-		mods = "ALT",
-		key = "RightArrow",
-		action = wezterm.action.AdjustPaneSize({ "Right", 3 }),
-	},
-	{
-		mods = "ALT",
-		key = "DownArrow",
-		action = wezterm.action.AdjustPaneSize({ "Down", 3 }),
-	},
-	{
-		mods = "ALT",
-		key = "UpArrow",
-		action = wezterm.action.AdjustPaneSize({ "Up", 3 }),
-	},
-	{
-		mods = "LEADER",
-		key = "z",
-		action = wezterm.action.TogglePaneZoomState,
-	},
+    {
+        mods = "ALT",
+        key = "c",
+        action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+        mods = "ALT",
+        key = "x",
+        action = wezterm.action.CloseCurrentPane({ confirm = true }),
+    },
+    -- {
+    -- 	mods = "ALT|SHIFT",
+    -- 	key = "h",
+    -- 	action = wezterm.action.ActivateTabRelative(-1),
+    -- },
+    -- {
+    -- 	mods = "ALT|SHIFT",
+    -- 	key = "l",
+    -- 	action = wezterm.action.ActivateTabRelative(1),
+    -- },
+    {
+        mods = "ALT",
+        key = "\\",
+        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        mods = "ALT",
+        key = "-",
+        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        mods = "ALT",
+        key = "h",
+        action = wezterm.action.ActivatePaneDirection("Left"),
+    },
+    {
+        mods = "ALT",
+        key = "j",
+        action = wezterm.action.ActivatePaneDirection("Down"),
+    },
+    {
+        mods = "ALT",
+        key = "k",
+        action = wezterm.action.ActivatePaneDirection("Up"),
+    },
+    {
+        mods = "ALT",
+        key = "l",
+        action = wezterm.action.ActivatePaneDirection("Right"),
+    },
+    {
+        mods = "ALT",
+        key = "LeftArrow",
+        action = wezterm.action.AdjustPaneSize({ "Left", 3 }),
+    },
+    {
+        mods = "ALT",
+        key = "RightArrow",
+        action = wezterm.action.AdjustPaneSize({ "Right", 3 }),
+    },
+    {
+        mods = "ALT",
+        key = "DownArrow",
+        action = wezterm.action.AdjustPaneSize({ "Down", 3 }),
+    },
+    {
+        mods = "ALT",
+        key = "UpArrow",
+        action = wezterm.action.AdjustPaneSize({ "Up", 3 }),
+    },
+    {
+        mods = "LEADER",
+        key = "z",
+        action = wezterm.action.TogglePaneZoomState,
+    },
 }
 
 config.window_background_opacity = 0.8
@@ -113,12 +113,12 @@ config.window_background_opacity = 0.8
 -- }
 
 for i = 0, 9 do
-	-- leader + number to activate that tab
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = wezterm.action.ActivateTab(i),
-	})
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(i),
+    })
 end
 
 -- tab bar
@@ -129,25 +129,25 @@ config.tab_and_split_indices_are_zero_based = true
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
-	local SOLID_LEFT_ARROW = ""
-	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-	local prefix = ""
+    local SOLID_LEFT_ARROW = ""
+    local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+    local prefix = ""
 
-	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	end
+    if window:leader_is_active() then
+        prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+    end
 
-	if window:active_tab():tab_id() ~= 0 then
-		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-	end -- arrow color based on if tab is first pane
+    if window:active_tab():tab_id() ~= 0 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+    end -- arrow color based on if tab is first pane
 
-	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
-		{ Text = prefix },
-		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW },
-	}))
+    window:set_left_status(wezterm.format({
+        { Background = { Color = "#b7bdf8" } },
+        { Text = prefix },
+        ARROW_FOREGROUND,
+        { Text = SOLID_LEFT_ARROW },
+    }))
 end)
 
 return config
