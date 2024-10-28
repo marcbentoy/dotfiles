b438ba8d7880b0e75bb6d46aab2402997c6051b5
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-10-28 13:55:30 +0800

    format

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -14,10 +14,10 @@ vim.opt.termguicolors = true
 ---
 --- General
 ---
-opt.mouse = "a"			-- Enable mouse support
-opt.clipboard = "unnamedplus" 	-- Copy/paste to system clipboard
-opt.swapfile = false 		-- Don't use swapfile
-opt.completeopt = "menuone,noinsert,noselect" 	-- Don't use swapfile
+opt.mouse = "a"                               -- Enable mouse support
+opt.clipboard = "unnamedplus"                 -- Copy/paste to system clipboard
+opt.swapfile = false                          -- Don't use swapfile
+opt.completeopt = "menuone,noinsert,noselect" -- Don't use swapfile
 
 ---
 --- Author
@@ -46,27 +46,27 @@ opt.cursorline = true
 ---
 --- Tabs, indent
 ---
-opt.expandtab = true 	-- Use spaces instead of tabs
-opt.shiftwidth = 4 	-- Shift 4 spaces when tabbed
-opt.tabstop = 4 	-- 1 tab == 4 spaces
-opt.softtabstop = 4 	-- Tab key delete 4 spaces
-opt.smartindent = true 	-- Auto indent new lines
+opt.expandtab = true   -- Use spaces instead of tabs
+opt.shiftwidth = 4     -- Shift 4 spaces when tabbed
+opt.tabstop = 4        -- 1 tab == 4 spaces
+opt.softtabstop = 4    -- Tab key delete 4 spaces
+opt.smartindent = true -- Auto indent new lines
 
 ---
 --- Memory/CPU
 ---
-opt.hidden = true 	-- Enable background buffers
-opt.history = 100 	-- Remember n lines of history
-opt.lazyredraw = true 	-- Faster scrolling
-opt.updatetime = 700 	-- ms to wait to trigger an event
+opt.hidden = true     -- Enable background buffers
+opt.history = 100     -- Remember n lines of history
+opt.lazyredraw = true -- Faster scrolling
+opt.updatetime = 700  -- ms to wait to trigger an event
 
 ---
 --- Nerd Commenter
 ---
 -- Adds a space after comment
-g.NERDSpaceDelims = 1 	
+g.NERDSpaceDelims = 1
 -- Adds a space after comment
-g.NERDTrimTrailingWhitespace = 1 	
+g.NERDTrimTrailingWhitespace = 1
 
 
 ---
@@ -77,33 +77,33 @@ opt.shortmess:append("sI")
 
 -- Disable builtin plugins
 local disabled_built_ins = {
-	"2html_plugin",
-	"getscript",
-	"getscriptPlugin",
-	"gzip",
-	"logipat",
-	"netrw",
-	"netrwPlugin",
-	"netrwSettings",
-	"netrwFileHandlers",
-	"matchit",
-	"tar",
-	"tarPlugin",
-	"rrhelper",
-	"spellfile_plugin",
-	"vimball",
-	"vimballPlugin",
-	"zip",
-	"zipPlugin",
-	"tutor",
-	"rplugin",
-	"synmenu",
-	"optwin",
-	"compiler",
-	"bugreport",
-	"ftplugin",
+    "2html_plugin",
+    "getscript",
+    "getscriptPlugin",
+    "gzip",
+    "logipat",
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "matchit",
+    "tar",
+    "tarPlugin",
+    "rrhelper",
+    "spellfile_plugin",
+    "vimball",
+    "vimballPlugin",
+    "zip",
+    "zipPlugin",
+    "tutor",
+    "rplugin",
+    "synmenu",
+    "optwin",
+    "compiler",
+    "bugreport",
+    "ftplugin",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-	g["loaded_" .. plugin] = 1
+    g["loaded_" .. plugin] = 1
 end
