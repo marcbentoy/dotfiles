b8c9c7dd55c22185897668faa5c2648ea1e6e4af
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-14 17:02:04 +0800

    change theme to catppuccin

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,15 +1,15 @@
 return {
-	-- "catppuccin/nvim",
-	-- priority = 1000,
-	-- config = true,
-	-- opts = {},
-	-- config = function()
-	-- 	local transparent = true
+	"catppuccin/nvim",
+	priority = 1000,
+	config = true,
+	opts = {},
+	config = function()
+		local transparent = true
 
-	-- 	require("catppuccin").setup({
-	-- 		transparent_background = transparent,
-	-- 	})
+		require("catppuccin").setup({
+			transparent_background = transparent,
+		})
 
-	-- 	vim.cmd("colorscheme catppuccin-mocha")
-	-- end,
+		vim.cmd("colorscheme catppuccin-mocha")
+	end,
 }
