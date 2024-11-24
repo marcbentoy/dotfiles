1791ff59e62d563f89163861e5cac5c41cf2349b
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-24 21:16:04 +0800

    add mac background blur

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
-config.font_size = 12
+config.font_size = 14
+
+config.macos_window_background_blur = 12
 
 config.inactive_pane_hsb = {
     saturation = 0.2,
