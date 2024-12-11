d9462d80a0fac1cf04293fe8a25a48e46e35a363
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-11 09:00:01 +0800

    add obsidian config

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,3 +1,4 @@
 require('core/keymaps')
 require('core/options')
 require('lazy-init')
+require('core/workflows')
--- nvim/.config/nvim/lua/plugins/obsidian-nvim.lua
+++ nvim/.config/nvim/lua/plugins/obsidian-nvim.lua
@@ -1,15 +1,15 @@
 return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
+    -- lazy = true,
+    -- ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     -- event = {
     --     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     --     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
     --     -- refer to `:h file-pattern` for more examples
-    --     "BufReadPre Users/marcbentoy/personal/secondbrain/*.md",
-    --     "BufNewFile Users/marcbentoy/personal/secondbrain/*.md",
+    --     "BufReadPre " .. vim.fn.expand("~/personal/secondbrain/").. "*.md",
+    --     "BufNewFile " .. vim.fn.expand("~/personal/secondbrain/").. "*.md",
     -- },
     dependencies = {
         -- Required.
@@ -23,10 +23,6 @@ return {
                 name = "personal",
                 path = "~/personal/secondbrain",
             },
-            -- {
-            --   name = "work",
-            --   path = "~/vaults/work",
-            -- },
         },
 
         -- see below for full list of options 👇
@@ -52,6 +48,18 @@ return {
                 return require("obsidian").util.smart_action()
             end,
             opts = { buffer = true, expr = true },
+        },
+        ["<leader>od"] = {
+            action = function()
+                return require("obsidian").util.today()
+            end,
+            opts = { noremap = true, silent = true },
+        },
+        ["<leader>pi"] = {
+            action = function()
+                return require("obsidian").util.paste_img()
+            end,
+            opts = { noremap = true, silent = true },
         }
     },
 }
