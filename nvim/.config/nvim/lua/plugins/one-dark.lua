1701e6b2b16797fcd64f48615f234a114c870c9c
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-18 16:36:23 +0800

    added a randomized onedark color scheme when opening neovim

--- nvim/.config/nvim/lua/plugins/one-dark.lua
+++ nvim/.config/nvim/lua/plugins/one-dark.lua
@@ -1,8 +1,12 @@
 return {
     "navarasu/onedark.nvim",
     config = function()
+        math.randomseed(os.time())
+        local schemes = { "dark", "darker", "cool", "deep", "warm", "warmer" }
+        local selected = math.random(#schemes)
+
         require("onedark").setup {
-            style = "darker"
+            style = schemes[selected]
         }
         require("onedark").load()
     end
