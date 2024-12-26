ebdabd4bf6f7d086280748b3c478e9a5be34f972
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-26 12:59:39 +0800

    revert: gruvbox as color theme

--- nvim/.config/nvim/lua/plugins/one-dark.lua
+++ nvim/.config/nvim/lua/plugins/one-dark.lua
@@ -1,13 +1,13 @@
 return {
-    "navarasu/onedark.nvim",
-    config = function()
-        math.randomseed(os.time())
-        local schemes = { "dark", "darker", "cool", "deep", "warm", "warmer" }
-        local selected = math.random(#schemes)
+    -- "navarasu/onedark.nvim",
+    -- config = function()
+    --     math.randomseed(os.time())
+    --     local schemes = { "dark", "darker", "cool", "deep", "warm", "warmer" }
+    --     local selected = math.random(#schemes)
 
-        require("onedark").setup {
-            style = schemes[selected]
-        }
-        require("onedark").load()
-    end
+    --     require("onedark").setup {
+    --         style = schemes[selected]
+    --     }
+    --     require("onedark").load()
+    -- end
 }
