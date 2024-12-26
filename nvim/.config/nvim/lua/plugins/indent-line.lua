af4825eca006327c0ddf687fd08741f343770834
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-26 15:07:01 +0800

    up: indent chars

--- nvim/.config/nvim/lua/plugins/indent-line.lua
+++ nvim/.config/nvim/lua/plugins/indent-line.lua
@@ -5,15 +5,14 @@ return {
         opts = {},
         config = function() 
             local highlight = {
+                "RainbowBlue",
                 "RainbowRed",
                 "RainbowYellow",
-                "RainbowBlue",
                 "RainbowOrange",
                 "RainbowGreen",
                 "RainbowViolet",
                 "RainbowCyan",
             }
-
             local hooks = require "ibl.hooks"
             -- create the highlight groups in the highlight setup hook, so they are reset
             -- every time the colorscheme changes
@@ -28,7 +27,10 @@ return {
             end)
 
             vim.g.rainbow_delimiters = { highlight = highlight }
-            require("ibl").setup { scope = { highlight = highlight } }
+            require("ibl").setup { 
+                indent = { char = "┆" },
+                scope = { highlight = highlight, char = "│" } 
+            }
 
             hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
         end
