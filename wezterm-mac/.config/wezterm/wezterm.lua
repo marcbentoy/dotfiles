0240d09ce34e7aba7da8b34f81d69371d21532f6
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2025-01-04 15:29:18 +0800

    up: mac font size

--- wezterm-mac/.config/wezterm/wezterm.lua
+++ wezterm-mac/.config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
 
 config.macos_window_background_blur = 24
 
