57d0a33f526fe73f487e39e4181510925a8e520b
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2025-01-22 01:06:15 +0800

    feat: add raycast extension for executing applescript

---
+++ nature-scroll.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Nature's Scroll
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🔄
+
+# Documentation:
+# @raycast.description Toggles natural scrolling of trackpad
+
+osascript toggle-natural-scrolling.applescript
+echo "Natural Scrolling Toggled 📜"
+
