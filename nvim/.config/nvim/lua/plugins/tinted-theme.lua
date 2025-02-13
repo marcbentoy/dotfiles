498f68b74752459a9b99b3ac810c3985f59864e1
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2025-02-13 10:56:29 +0800

    up: theme

--- nvim/.config/nvim/lua/plugins/tinted-theme.lua
+++ nvim/.config/nvim/lua/plugins/tinted-theme.lua
@@ -1,8 +1,8 @@
 return {
-    {
-        "tinted-theming/tinted-vim",
-        config = function()
-            vim.cmd.colorscheme('base16-ayu-dark')
-        end
-    }
+    -- {
+    --     "tinted-theming/tinted-vim",
+    --     config = function()
+    --         vim.cmd.colorscheme('base16-ayu-dark')
+    --     end
+    -- }
 }
