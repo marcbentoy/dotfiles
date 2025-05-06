4728d62149c282ed55bd7a8297acbe4b1de83ea6
Marc Vincent Bentoy <marcvincentbentoy@benifex.com>
2025-05-06 20:41:41 +0800

    refactor: remove obsidian.nvim

--- nvim/.config/nvim/lua/plugins/obsidian-nvim.lua
+++ nvim/.config/nvim/lua/plugins/obsidian-nvim.lua
@@ -1,53 +1,53 @@
 return {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    -- lazy = true,
-    -- ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --     -- refer to `:h file-pattern` for more examples
-    --     "BufReadPre " .. vim.fn.expand("~/personal/secondbrain/").. "*.md",
-    --     "BufNewFile " .. vim.fn.expand("~/personal/secondbrain/").. "*.md",
+    -- "epwalsh/obsidian.nvim",
+    -- version = "*", -- recommended, use latest release instead of latest commit
+    -- -- lazy = true,
+    -- -- ft = "markdown",
+    -- -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- -- event = {
+    -- --     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- --     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- --     -- refer to `:h file-pattern` for more examples
+    -- --     "BufReadPre " .. vim.fn.expand("~/personal/secondbrain/").. "*.md",
+    -- --     "BufNewFile " .. vim.fn.expand("~/personal/secondbrain/").. "*.md",
+    -- -- },
+    -- dependencies = {
+    --     -- Required.
+    --     "nvim-lua/plenary.nvim",
+
+    --     -- see below for full list of optional dependencies 👇
     -- },
-    dependencies = {
-        -- Required.
-        "nvim-lua/plenary.nvim",
+    -- opts = {
+    --     workspaces = {
+    --         {
+    --             name = "personal",
+    --             path = "~/personal/secondbrain",
+    --         },
+    --     },
 
-        -- see below for full list of optional dependencies 👇
-    },
-    opts = {
-        workspaces = {
-            {
-                name = "personal",
-                path = "~/personal/secondbrain",
-            },
-        },
-
-        -- see below for full list of options 👇
-    },
-    mappings = {
-        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        ["gf"] = {
-            action = function()
-                return require("obsidian").util.gf_passthrough()
-            end,
-            opts = { noremap = false, expr = true, buffer = true },
-        },
-        -- Toggle check-boxes.
-        ["<leader>ch"] = {
-            action = function()
-                return require("obsidian").util.toggle_checkbox()
-            end,
-            opts = { buffer = true },
-        },
-        -- Smart action depending on context, either follow link or toggle checkbox.
-        ["<cr>"] = {
-            action = function()
-                return require("obsidian").util.smart_action()
-            end,
-            opts = { buffer = true, expr = true },
-        },
-    },
+    --     -- see below for full list of options 👇
+    -- },
+    -- mappings = {
+    --     -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    --     ["gf"] = {
+    --         action = function()
+    --             return require("obsidian").util.gf_passthrough()
+    --         end,
+    --         opts = { noremap = false, expr = true, buffer = true },
+    --     },
+    --     -- Toggle check-boxes.
+    --     ["<leader>ch"] = {
+    --         action = function()
+    --             return require("obsidian").util.toggle_checkbox()
+    --         end,
+    --         opts = { buffer = true },
+    --     },
+    --     -- Smart action depending on context, either follow link or toggle checkbox.
+    --     ["<cr>"] = {
+    --         action = function()
+    --             return require("obsidian").util.smart_action()
+    --         end,
+    --         opts = { buffer = true, expr = true },
+    --     },
+    -- },
 }
